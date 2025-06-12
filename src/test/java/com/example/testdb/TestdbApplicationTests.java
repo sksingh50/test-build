b24fdc685d412e627a2bbebfffc909bf8976e304
shaurabh <shaurabhs50@gmail.com>
2025-06-12 12:23:29 +0530

    Update TestdbApplicationTests.java

--- src/test/java/com/example/testdb/TestdbApplicationTests.java
+++ src/test/java/com/example/testdb/TestdbApplicationTests.java
@@ -4,11 +4,11 @@ package com.example.testdb;
 // import com.example.testdb.controller.Repository;
 // import com.example.testdb.controller.TestDB;
 // import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.Test;
 // import org.mockito.InjectMocks;
 // import org.mockito.Mock;
 // import org.mockito.MockitoAnnotations;
-// import org.springframework.boot.test.context.SpringBootTest;
+ import org.springframework.boot.test.context.SpringBootTest;
 // import org.springframework.http.ResponseEntity;
 
 // import java.util.Arrays;
